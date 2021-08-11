78f235f3152d681d647f2081b718df1a6885c6a1
Adam Nash <adam.nash@live.com>
2021-08-11 12:42:30 -0600

    refactor

---
+++ MVCTC/Testing/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int OnSale { get; set; }
+        public int StockLevel { get; set; }
+        public int CategoryID { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public string orderby{get; set;}
+    }
+}
---
+++ MVCTC/Testing/ProductRepository.cs
@@ -0,0 +1,74 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Testing.Models;
+
+namespace Testing
+{
+
+    
+        public class ProductRepository : IProductRepository
+        {
+
+            private readonly IDbConnection _conn;
+
+        public int StockLevel { get; private set; }
+
+        public ProductRepository(IDbConnection conn)
+            {
+                _conn = conn;
+            }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _conn.Query<Product>("Select * from Products;");
+        }
+
+        public Product GetProduct(int id)
+        {
+            return _conn.QuerySingle<Product>("select * from Products where productID = @id", new { id = id });
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            _conn.Execute("UPDATE products SET Name = @name, Price = @price, StockLevel = @StockLevel WHERE ProductID = @id",
+                new { name = product.Name, price = product.Price, StockLevel = product.StockLevel, id = product.ProductID });
+        }
+
+        public void InsertProduct(Product productToInsert)
+        {
+            _conn.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID, StockLevel) VALUES (@name, @price, @categoryID, @StockLevel);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID, StockLevel = productToInsert.StockLevel});
+        }
+
+        public IEnumerable<Category> GetCategories()
+        {
+            return _conn.Query<Category>("SELECT * FROM categories;");
+        }
+        public Product AssignCategory()
+        {
+            var categoryList = GetCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+
+            return product;
+        }
+        public void DeleteProduct(Product product)
+        {
+            _conn.Execute("DELETE FROM REVIEWS WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Sales WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+            _conn.Execute("DELETE FROM Products WHERE ProductID = @id;",
+                                       new { id = product.ProductID });
+        }
+
+
+
+    }
+
+
+}
