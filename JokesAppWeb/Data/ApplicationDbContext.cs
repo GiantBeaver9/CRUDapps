3285fc03694ec59c2d192bf6467d52373e17aa3e
Adam Nash <adam.nash@live.com>
2021-08-11 09:39:28 -0600

    initial commit

---
+++ JokesAppWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using JokesAppWeb.Models;
+
+namespace JokesAppWeb.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesAppWeb.Models.Joke> Joke { get; set; }
+    }
+}
